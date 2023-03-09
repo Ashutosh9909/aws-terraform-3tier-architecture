962197780da59b28631b37fdb2abf498a0bfa804
Ashutosh9909 <apati9909@gmail.com>
2023-03-09 12:47:20 +0530

    Updated subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 # Creating Public Subnet
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
@@ -9,7 +9,7 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
