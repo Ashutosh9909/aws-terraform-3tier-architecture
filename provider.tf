eb24dee6911fe7fd106196da7cd221c04cbc0b74
ASHUTOSH PATI <104639044+Ashutosh9909@users.noreply.github.com>
2023-03-09 16:51:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAQGPM3HQWD33S47LR"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
