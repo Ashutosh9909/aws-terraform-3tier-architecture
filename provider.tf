1c58c1142c7c38b2c0783bf1c66cade163e8d58b
ASHUTOSH PATI <104639044+Ashutosh9909@users.noreply.github.com>
2023-03-09 19:27:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "4.57.1"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region_name
+  access_key = ""
+  secret_key = ""
 }
