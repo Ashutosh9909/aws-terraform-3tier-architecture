bfcbcba304bc125eed1d6bc9d4d578224ed97b0c
Ashutosh9909 <apati9909@gmail.com>
2023-03-09 12:55:04 +0530

    Updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,27 @@
 # Creating EC2 Web-Server
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
   key_name = "aws"
-  subnet_id = aws_subnet.public[count.index].id
+  subnet_id = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
-  count = 2
 
   tags = {
-    Name = "WebServer"
+    Name = "WebServer1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-09ba48996007c8b50"
+  instance_type = "t2.micro"
+  key_name = "aws"
+  subnet_id = aws_subnet.public2.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "WebServer2"
   }
 }
 
