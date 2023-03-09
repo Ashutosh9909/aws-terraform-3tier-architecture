d01f4907e0a32a27eb1bef7b3270132919712247
ASHUTOSH PATI <104639044+Ashutosh9909@users.noreply.github.com>
2023-03-09 19:24:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,39 +1,27 @@
-# Creating EC2 Web-Server
-resource "aws_instance" "web1" {
-  ami           = "ami-09ba48996007c8b50"
-  instance_type = "t2.micro"
+# Creating EC2 Web Server
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
   key_name = "aws"
-  subnet_id = aws_subnet.public1.id
+  subnet_id = aws_subnet.public[count.index].id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
+  count = 2
 
   tags = {
-    Name = "WebServer1"
+    Name = "Web-${count.index + 1}"
   }
 }
 
-resource "aws_instance" "web2" {
-  ami           = "ami-09ba48996007c8b50"
-  instance_type = "t2.micro"
-  key_name = "aws"
-  subnet_id = aws_subnet.public2.id
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "WebServer2"
-  }
-}
-
-# Creating EC2 DB-Server
+# Creating EC2 DB Server
 resource "aws_instance" "db" {
-  ami           = "ami-09ba48996007c8b50"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   key_name = "aws"
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
 
   tags = {
-    Name = "DB Server"
+    Name = "DB"
   }
 }
